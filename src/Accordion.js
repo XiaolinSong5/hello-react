3acd9bfb1e8869ccd5e09cda8253fde3f4ffbb5a
Xiaolin <xiaolin.song@benerail.com>
2026-01-31 18:08:45 +0100

    useState

--- src/Accordion.js
+++ src/Accordion.js
@@ -1,11 +1,19 @@
 import './Accordion.css';
+import {useState} from "react";
 
 export default function Accordion(props) {
+    const [open, setOpen] = useState(false);
     console.log({props});
+    const handleClick = () => {
+        console.log('This was clicked');
+        setOpen(true);
+    }
 //return (<div>this is an accordion</div>);
     return (
         <div className="accordion">
-            <div className="label-text">{props.labelText}</div>
+            <div className="label-text" onClick={handleClick}>{props.labelText}</div>
+            {!!open &&
             <div>{props.children}</div>
+            }
         </div>);
 }
